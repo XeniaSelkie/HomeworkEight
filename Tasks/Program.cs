b883531f03d0b0ba62065e83ee1c9f8ea8768fb2
Sana <mypostoffice94@gmail.com>
2023-03-13 19:45:31 +0300

    Add Homework 1.0

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -7,25 +7,25 @@
 		case 54: Task54(); break;
 		case 56: Task56(); break;
 		case 58: Task58(); break;
+		case 60: Task60(); break;
 	}
 }
 
 
 void Task54() // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 {
-    int[,] array = CreateTwoDimensionIntArray(ReadInt("первое измерение"), ReadInt("второе измерение"));
+    int[,] array = CreateTwoDimensionIntArray(ReadInt("первое измерение массива"), ReadInt("второе измерение массива"));
 	Console.WriteLine(TwoDimensionIntArrayToString(array));
     Console.WriteLine(LineSortDescending(array));
 }
 
 void Task56() // Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 {
-	int[,] array = CreateTwoDimensionIntArray(3, 7);
+	int[,] array = CreateTwoDimensionIntArray(ReadInt("первое измерение массива"), ReadInt("второе измерение массива"));
 	Console.WriteLine(TwoDimensionIntArrayToString(array));
-	int firstLineNumber = 0;
-	int secondLineNumber = 1;
-	int ThirdLineNumber = 2;
-	ComparisonSum((SumLineElements(firstLineNumber, array)), (SumLineElements(secondLineNumber, array)), (SumLineElements(ThirdLineNumber, array)));
+	int i = 0;
+	MinSumLineElements(i, array);
+		
 
 }
 
@@ -41,6 +41,18 @@ void Task58() // Задайте две матрицы. Напишите прог
 	}
 	else 
 	Console.WriteLine("Матрицы не согласованы, умножение невозможно. Попробуйте ввести матрицы так, чтобы количеcтво столбцов первой матрицы было равно количеству строк второй матрицы");	
+}
+
+void Task60() // Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+{
+	int x = ReadInt("первое измерение массива");
+	int y = ReadInt("второе измерение массива");
+	int z = ReadInt("третье измерение массива");
+	int i = x + y + z;
+	int[] array = CreateArrayUniqueElement(i);
+
+	// int[,,] array = CreateThreeDimensionIntArray(ReadInt("первое измерение массива"), ReadInt("второе измерение массива"), ReadInt("третье измерение массива"));
+	// Console.WriteLine(ThreeDimensionIntArrayToString(array));
 }
 
 int ReadInt(string argument)
@@ -112,31 +124,27 @@ string LineSortDescending(int[,] array)
 		return TwoDimensionIntArrayToString(array);
     }
 
-int SumLineElements(int i, int[,] array)
+void MinSumLineElements(int line, int[,] array)
 {
 	int sum = 0;
+	int min = 1000;
+	int minLine = 0;
 
-	for (int j = 0; j < array.GetLength(1); j++)
-	{
-		sum = sum + array[i,j];
+	for (int i = 0; i < array.GetLength(0); i++)
+	{	
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			sum = sum + array[i,j];
+		}
+		Console.WriteLine($"Сумма элементов строки {i} равняется {sum}");	
+		if (sum < min)
+		{
+			min = sum;
+			minLine = i+1;
+		}	
 	}
-
-	Console.WriteLine($"Сумма строки {i} равняется {sum}");
-
-	return (sum);
-}
-
-void ComparisonSum(int a, int b, int c)
-{
-	if (a < b && a < c)
-	Console.WriteLine($"Строка с наименьшей суммой элементов - первая");
-	else
-	{
-		if (b < c && b < a)
-		Console.WriteLine($"Строка с наименьшей суммой элементов - вторая");
-		else
-		Console.WriteLine($"Строка с наименьшей суммой элементов - третья");
-	}
+	Console.WriteLine();
+	Console.WriteLine($"Наименьшая строка - {minLine}-я, сумма её элементов  равняется {min}");
 }
 
 void MultipleMatrices(int[,] firstMatrix, int[,] secondMatrix)
@@ -158,4 +166,47 @@ void MultipleMatrices(int[,] firstMatrix, int[,] secondMatrix)
 		}
 		Console.WriteLine();
 	}
+}
+
+// int[,,] CreateThreeDimensionIntArray(int firstLength, int secondLength, int thirdLength)
+// {
+// 	int[,,] result = new int[firstLength, secondLength, thirdLength];
+// 	Random rnd = new Random();
+
+// 	for (int x = 0; x < result.GetLength(0); x++)
+// 	{
+// 		for (int y = 0; y < result.GetLength(1); y++)
+// 		{
+// 			for (int z = 0; z < result.GetLength(2); z++)
+// 			{
+// 				result[x,y,z] = rnd.Next(10, 100);
+// 			}	
+// 		}
+// 	}
+
+// 	return result;
+// }
+
+int[] CreateArrayUniqueElement(int firstLength)
+{
+	int[] result = new int[firstLength];
+	Random rnd = new Random();
+	
+	for (int i = 0; i < result.GetLength(0); i++)
+	{
+		int count = 1;
+		result[i] = rnd.Next(10, 100);
+		if (i > 0)
+		{
+			while (count <= i)
+			{
+				if (result[i] == result[i-count]) 
+					result[i] = rnd.Next(10, 100);
+				count = count+1;
+			}
+		Console.WriteLine(result[i]);
+		count = 0;
+		}
+	}	
+	return result;
 }
